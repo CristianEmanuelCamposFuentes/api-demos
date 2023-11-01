c0022f2fa56f860896531f15f1ecbd38e48a0d86
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-10-31 23:54:28 -0300

    Actualizando dependencias

---
+++ app/src/main/java/com/example/myapplication/A1_AppiumBasicsApp1.java
@@ -0,0 +1,11 @@
+package com.example.myapplication;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class A1_AppiumBasicsApp1 {
+    public static void main(String[] args) throws MalformedURLException {
+        A0_UiAutomator2Options options = new A0_UiAutomator2Options();
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"));
+    }
+}
